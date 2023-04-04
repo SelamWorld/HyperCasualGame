f9c8be36e62d35a2b4c546b44d9f7569d9592453
halil ibrahim <halil199010@hotmail.com>
2023-04-04 19:23:13 +0300

    added character movement

--- Assets/Scripts/CharMove.cs
+++ Assets/Scripts/CharMove.cs
@@ -1,11 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharMove : MonoBehaviour
 {
-public  float CharSpeed;
-public float limitX;            
+public  float CharSpeed,xSpeed;
+public float limitX;                   
 private  float _CurrentCharSpeed;
     
     void Start()
@@ -19,8 +20,18 @@ private  float _CurrentCharSpeed;
     {
         float newX = 0;
         float touchXDelta=0;
+        if(Input.touchCount>0 && Input.GetTouch(0).phase==TouchPhase.Moved){
+            touchXDelta = Input.GetTouch(0).deltaPosition.x/Screen.width;
+            
+        }else if(Input.GetMouseButton(0)) {
+            touchXDelta = Input.GetAxis("Mouse X");
+        }
+        newX=transform.position.x+ xSpeed*touchXDelta*Time.deltaTime;
+        newX = Mathf.Clamp(newX,-limitX, limitX);
+
+
         // caharacter movement
-        Vector3 NewPosition = new Vector3(transform.position.x,transform.position.y,transform.position.z+_CurrentCharSpeed*Time.deltaTime);
+        Vector3 NewPosition = new Vector3(newX,transform.position.y,transform.position.z+_CurrentCharSpeed*Time.deltaTime);
         transform.position = NewPosition;
     }
 }
